132df2b1b68928b6cce9d23fc3895dcd4519835e
stamplong <longzitan@126.com>
2017-04-12 13:18:06 +0800

    0412py
    python Exersise

---
+++ 16E10.py
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "i'm split\n on a line"
+backslash_cat = "i'm \\ a \\ cat"
+
+fat_cat ="""
+i'll do a list:* cat food
+\t* fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
---
+++ 17E11.py
@@ -0,0 +1,8 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "so, you're %s old %r tall and %s  heavry."%(age,height,weight)
