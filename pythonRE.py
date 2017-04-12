06116bf46791ded8a98092c549a9dc4f7b41abcd
stamplong <longzitan@126.com>
2017-04-12 22:38:49 +0800

    python

--- pythonRE.py
+++ pythonRE.py
@@ -5,3 +5,22 @@ if matchobj:
     print "matchobj.group():",matchobj.group()
 else:
     print "no match"
+dic = {'longz':"123",'zi':"4n56"}
+for i in dic:
+    print "dic[%s]"%i,dic[i]
+for (k,v)in dic.items():
+    print "dic[%s]"%k,v
+
+print dic.keys()
+print dic.items()
+print dic.values()
+print dic.get("longzitan")
+print dic.get("longz")
+print dic
+print dic.update({"123":"456"})
+print dic
+print dic.setdefault("123","789")
+print dic.setdefault("333","444")
+print dic
+print dic.pop("longz")
+print dic
