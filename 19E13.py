0c1bbd2199fc2710773243cce3331a910d8b9445
stamplong <longzitan@126.com>
2017-04-13 16:37:45 +0800

    Revert "from sys import argv"
    This reverts commit 2e1f5dce1802d20d110225e65751f409538984c7.

--- 19E13.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-s1 = argv[0]
-first = argv[1]
-second = argv[2]
-third = argv[3]
-print "The script is called :",s1
-print "your first variable is :",first
-print "your second variable is:",second
-print "your third variable is",third
