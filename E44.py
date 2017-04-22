40e78bf125f4e460d29934ef5cf063a169b2bd39
longzitan <longzitan16@163.com>
2017-04-22 21:44:09 +0800

    类继承

---
+++ E44.py
@@ -0,0 +1,121 @@
+class Parent(object):
+
+    def override(self):
+        print "PARENT override()"
+
+class Child(Parent):
+
+    def override(self):
+        print "CHILD override()"
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
+print "---"*20
+class Parent(object):
+
+    def altered(self):
+        print "PARENT altered()"
+
+class Child(Parent):
+
+    def altered(self):
+        print "CHILD, BEFORE PARENT altered()"
+        super(Child, self).altered()
+        print "CHILD, AFTER PARENT altered()"
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
+
+print "---"*20
+class Parent(object):
+
+    def study(self):
+        print "PARENT altered()11111111"
+        print "1234"
+
+class child1(Parent):
+    def study(self):
+        super(child1,self).study()
+        print "*****4567***"
+
+view = Parent()
+view1 = child1()
+print "*******"*15
+view.study()
+view1.study()
+
+print "===="*16
+
+class Parent(object):
+
+    def override(self):
+        print "PARENT override()"
+
+    def implicit(self):
+        print "PARENT implicit()"
+
+    def altered(self):
+        print "PARENT altered()"
+
+class Child(Parent):
+
+    def override(self):
+        print "CHILD override()"
+
+    def altered(self):
+        print "CHILD, BEFORE PARENT altered()"
+        super(Child, self).altered()
+        print "CHILD, AFTER PARENT altered()"
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
+
+print "****"*15
+
+class Other(object):
+
+    def override(self):
+        print "OTHER override()"
+
+    def implicit(self):
+        print "OTHER implicit()"
+
+    def altered(self):
+        print "OTHER altered()"
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print "CHILD override()"
+
+    def altered(self):
+        print "CHILD, BEFORE OTHER altered()"
+        self.other.altered()
+        print "CHILD, AFTER OTHER altered()"
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
